5a0e7b4048e205ecfe2187a0ebb71eb683ca50fd
artem.buhera <artem.bugera@gmail.com>
2020-10-12 16:34:47 +0200

    comments

--- projekt1/z3.cpp
+++ projekt1/z3.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    // wczyt
     char* masa_ciala = (char*) malloc(sizeof(char));
     char* wzrost = (char*) malloc(sizeof(char));
 
     cout << "masa ciała [kg]: ";
     cin >> masa_ciala;
-
     cout << "wzrost [m]: ";
     cin >> wzrost;
 
+    // zamiana na doubly i obliczenie bmi
     double bmi = atof(masa_ciala) / (atof(wzrost) * atof(wzrost));
+
+    // printowanie w zależności od wartości bmi
     cout << "bmi: " << bmi << " - ";
     if (bmi < 16)        cout << "wygłodzenie";
     else if (bmi < 17)   cout << "wychudzenie";
