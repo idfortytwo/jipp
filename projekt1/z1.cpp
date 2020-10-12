7e2a69c8a16355290efaa2f3e7a1a5e91744af67
artem.buhera <artem.bugera@gmail.com>
2020-10-12 12:21:35 +0200

    Zadanie 1

--- projekt1/z1.cpp
+++ projekt1/z1.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << "hello" << endl;
-    return 0;
+    int* input = (int*) malloc(sizeof(int));
+    int inputLen = argc-1;
+    
+    // wpisywanie tekstowych argumentów argv do intowego arrayu input
+    for (int i = 0; i < argc; i++)
+        input[i] = atoi(argv[i+1]);
+
+    // sortowanie arrayu input
+    sort(input, input + inputLen);
+
+    cout << "out = ";
+    for (int i = 0; i < inputLen; i++)
+        cout << input[i] << " ";
+    cout << endl;
 }
