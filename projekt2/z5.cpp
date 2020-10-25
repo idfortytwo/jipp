04d1ceefe5319bdb530571e3abf043338870c42a
artem.buhera <artem.bugera@gmail.com>
2020-10-25 20:18:15 +0100

    testy dla zadania 5

--- projekt2/z5.cpp
+++ projekt2/z5.cpp
@@ -8,12 +8,28 @@ template <class T> void swap(T *a, T *b) {
     *b = temp;
 }
 
+template <class T>
+void testSwap(T a, T b) {
+    T aAfter = a, bAfter = b;
+    swap(&aAfter, &bAfter);
+
+    // sprawdza czy wartości pozamienianych aAfter i bAfter są odpowiednio równe b i a
+    if (aAfter == b && bAfter == a) 
+        cout << "działa" << endl;
+    else 
+        cout << "nie działa" << endl;
+}
+
 int main() {
     int a = 30, b = 500;
 
     cout << "before: a=" << a << " b=" << b << endl;
     swap(&a, &b);
-    cout << "after:  a=" << a << " b=" << b << endl << endl;
+    cout << "after:  a=" << a << " b=" << b << endl;
+
+    cout << "testowanie swap dla intów 30 i 500: ";
+    testSwap(a, b);
+    cout << endl;
 
 
     string c = "First", d = "sEcond";
@@ -21,4 +37,7 @@ int main() {
     cout << "before: c=" << c << " d=" << d << endl;
     swap(&c, &d);
     cout << "after:  c=" << c << " d=" << d << endl;
+
+    cout << "testowanie swap dla stringów \"First\" i \"sEcond\": ";
+    testSwap(c, d);
 }
